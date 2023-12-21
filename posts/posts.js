966648547910771405ac2554473e6f14e4815d68
Isaac Cortes Hernandez <isaaccortes014@gmail.com>
2023-12-21 12:09:22 -0800

    profile sidebar and post styling

--- posts/posts.js
+++ posts/posts.js
@@ -1,3 +1,14 @@
 /* Posts Page JavaScript */
 
-"use strict";
+'use strict';
+
+// fetch('http://microbloglite.us-east-2.elasticbeanstalk.com/api/posts', {
+//   method: 'POST',
+//   headers: {
+//     Accept: 'application/json',
+//     Authorization:
+//       'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InN0cmluZyIsImlhdCI6MTcwMzE4MDUzMCwiZXhwIjoxNzAzMjY2OTMwfQ.Ap2zYPVTc6UYGFnlMQwgS5Z6uZyHQoECMOooU0KMqOk',
+//     'Content-Type': 'application/json',
+//   },
+//   body: JSON.stringify(formData),
+// });
