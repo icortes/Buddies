f35d946c4129a95cf07e15c5fa2632aaab305bfc
Isaac Cortes Hernandez <isaaccortes014@gmail.com>
2023-12-21 13:53:54 -0800

    user info on side bar

--- posts/posts.js
+++ posts/posts.js
@@ -2,13 +2,39 @@
 
 'use strict';
 
-// fetch('http://microbloglite.us-east-2.elasticbeanstalk.com/api/posts', {
-//   method: 'POST',
-//   headers: {
-//     Accept: 'application/json',
-//     Authorization:
-//       'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InN0cmluZyIsImlhdCI6MTcwMzE4MDUzMCwiZXhwIjoxNzAzMjY2OTMwfQ.Ap2zYPVTc6UYGFnlMQwgS5Z6uZyHQoECMOooU0KMqOk',
-//     'Content-Type': 'application/json',
-//   },
-//   body: JSON.stringify(formData),
-// });
+let loginData = getLoginData();
+
+//on window load
+onload = async () => {
+  let userResponse = await fetch(
+    `http://microbloglite.us-east-2.elasticbeanstalk.com/api/users/${loginData.username}`,
+    {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        Authorization: `Bearer ${loginData.token}`,
+        'Content-Type': 'application/json',
+      },
+    }
+  );
+
+  let user = await userResponse.json();
+  console.log(user);
+
+  document.getElementById('userFullName').textContent = user.fullName;
+  document.getElementById('userName').textContent = `@${user.username}`;
+  document.getElementById('userBio').textContent = user.bio;
+
+  // let postsResponse = await fetch(
+  //   'http://microbloglite.us-east-2.elasticbeanstalk.com/api/posts',
+  //   {
+  //     method: 'POST',
+  //     headers: {
+  //       Accept: 'application/json',
+  //       Authorization: `Bearer ${loginData.token}`,
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify(formData),
+  //   }
+  // );
+};
